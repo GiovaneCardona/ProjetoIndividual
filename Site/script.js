45ecb8079c58e2f989bea46ff7632c1e9bfb97ed
GiovaneCardona <giovanecardona@sptech.school>
2023-11-13 12:44:33 -0300

    limpando código JS carrossel

--- Site/script.js
+++ Site/script.js
@@ -21,15 +21,12 @@ function nextImage() {
         document.getElementById("img_malong").style.color = "Red"
     }
     if (count == 3) {
-        document.getElementById("img_hugo").style.color = "white"
         document.getElementById("img_malin").style.color = "red"
         document.getElementById("img_malong").style.color = "white"
 
     }
     if (count == 4) {
-        document.getElementById("img_hugo").style.color = "white"
         document.getElementById("img_malin").style.color = "white"
-        document.getElementById("img_malong").style.color = "white"
         document.getElementById("img_jinkun").style.color = "red"
 
     }
