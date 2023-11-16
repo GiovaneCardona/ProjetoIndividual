87022cbbf176ddd231461f02e75a347770b568f8
GiovaneCardona <giovane.cardona@sptech.school>
2023-11-16 00:41:13 -0300

    Conectando login (funcionando)

--- ProjetoIndividual/Site/src/controllers/usuarioController.js
+++ ProjetoIndividual/Site/src/controllers/usuarioController.js
@@ -1,5 +1,5 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+// var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -15,12 +15,27 @@ function autenticar(req, res) {
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
+                    // transforma JSON em String
+
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        } else if (resultadoAutenticar.length == 0) {
+                        if (resultadoAutenticar.length > 0) {
+                            res.json({
+                                id: resultadoAutenticar[0].id,
+                                email: resultadoAutenticar[0].email,
+                                nome: resultadoAutenticar[0].nome,
+                                senha: resultadoAutenticar[0].senha,
+                                idade: resultadoAutenticar[0].idadeinicio
+
+                            });
+                        } else {
+                            res.status(204).json({ aquarios: [] });
+                        }
+
+                    } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
@@ -46,9 +61,9 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
 
     // Faça as validações dos valores
-    if(nome == undefined){
+    if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    }else if (email == undefined) {
+    } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
